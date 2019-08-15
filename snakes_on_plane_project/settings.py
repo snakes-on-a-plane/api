092eaa66b50f4a13f299c6d4d81093678def0518
JB <jb.tellez@gmail.com>
2019-08-15 12:07:35 -0700

    further removes Neville

--- snakes_on_plane_project/settings.py
+++ snakes_on_plane_project/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'passengers.apps.PassengersConfig',
-    'neville.apps.NevilleConfig',
 ]
 
 MIDDLEWARE = [
