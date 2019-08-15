5d1784b46f1ef49309e61a363ae434b4898efb52
JB <jb.tellez@gmail.com>
2019-08-15 12:12:52 -0700

    updates carry on tests

--- passengers/tests.py
+++ passengers/tests.py
@@ -1,26 +1,15 @@
 from django.test import TestCase
-from .models import Passenger
 from .models import CarryOn
 
-class NevilleTestCase(TestCase):
+class CarryOnTestCase(TestCase):
     def setUp(self):
         rake = CarryOn.objects.create(name="rake")
-        laptop = CarryOn.objects.create(name="laptop")
-        Passenger.objects.create(name="Neville", carry_on=rake, personal_item=laptop)
         
 
-    def test_neville_name(self):
-        neville = Passenger.objects.get(name = "Neville")
-        self.assertEqual(neville.name, "Neville")
+    def test_rake_name(self):
+        rake = CarryOn.objects.get(name = "rake")
+        self.assertEqual(rake.name, "rake")
 
-    # def test_neville_quote(self):
-    #     neville = Neville.objects.get(name = "Neville")
-    #     self.assertEqual(neville.quote, "ouch")
-
-    def test_neville_carry_on(self):
-        neville = Passenger.objects.get(name = "Neville")
-        self.assertEqual(neville.carry_on.name, "rake")
-
-    def test_neville_personal_item(self):
-        neville = Passenger.objects.get(name = "Neville")
-        self.assertEqual(neville.personal_item.name, "laptop")
+    def test_default_attack(self):
+        rake = CarryOn.objects.get(name = "rake")
+        self.assertEqual(rake.attack, 100)
