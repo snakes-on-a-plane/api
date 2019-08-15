098216da76cea677fb20aa03d6072d06637095b3
arsummers <aliyasummers1@gmail.com>
2019-08-15 15:47:01 -0700

    added money to mercedes

--- mercedes/models.py
+++ mercedes/models.py
@@ -4,6 +4,6 @@ from passengers.models import Passenger
 # Create your models here.
 class Mercedes(Passenger):
     quote = models.CharField(max_length=1024)
-
+    money = models.CharField(max_length=10, default=None)
     def __str__(self):
         return self.name
